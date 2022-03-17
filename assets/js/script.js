305dd4f6d48e7730fa75734c51266796ccc4585a
Kyle <kylejmbaylis@gmail.com>
2022-03-17 19:21:02 -0400

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,14 +12,38 @@ var startButton = document.getElementById("startGame");
 
 
 // main game code
-var codeGame = function() {
+var codeGameStart = function() {
     // window.alert(""); // test code to see if function and click work - it does
-    // decrease time by one on button click (needs to be every second)
+
+    /*
+    // decrease time by one on button click (needs to be every second) COUNTDOWN TIMER HERE
     timeRemain = timeRemain - 1;
-    timer = document.getElementById("time").innerHTML = "<p>" + timeRemain + "</p>";
+    */
+
+    // timer
+    var startCount = function() {
+        if (timeRemain <= -1) {
+        clearInterval(startCount);
+        return;
+    }
+        document.getElementById("time").innerHTML = "<p>" + timeRemain + "</p>";
+        timeRemain -= 1;     
+        if (timeRemain === 0) {
+            window.alert("Game over!");
+        }
+    }
+    
+    setInterval(startCount, 10);
+    // timer = document.getElementById("time").innerHTML = "<p>" + timeRemain + "</p>"; not neede?
+        
+    // to begin game
+    if (codeGameStart) {
+        toStart = document.getElementById("game").innerHTML = "<button id='startGame'>Ok</button>";
+    }
 }
 
 // to click start to begin the game
-startButton.addEventListener("click", codeGame); 
+var gameBegin = startButton.addEventListener("click", codeGameStart); 
+
 
 
