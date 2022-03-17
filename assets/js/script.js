649f6a9e6618cc1f43d62c3d2483c56a443a321e
Kyle <kylejmbaylis@gmail.com>
2022-03-17 17:21:29 -0400

    start button

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// create initial start button
+var toStart = document.getElementById("game").innerHTML = "<button id='startGame'>Start</button>";
+
+// for addEventListener to get button id
+var startButton = document.getElementById("startGame");
+
+// main game code?
+var codeGame = function() {
+    // window.alert(""); // test code to see if function and click work - it does
+}
+
+// startButton.addEventListener("click", codeGame); // to click start to begin the game
+
