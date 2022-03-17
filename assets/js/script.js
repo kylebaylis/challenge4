733392a1a0634c99c1c023087d58ab5ff6f1649e
Kyle <kylejmbaylis@gmail.com>
2022-03-17 17:44:14 -0400

    button click attached to timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,25 @@
+// time in timer at start
+var timeRemain = 60;
+
+// timer start number
+var timer = document.getElementById("time").innerHTML = "<p>" + timeRemain + "</p>";
+
 // create initial start button
 var toStart = document.getElementById("game").innerHTML = "<button id='startGame'>Start</button>";
 
 // for addEventListener to get button id
 var startButton = document.getElementById("startGame");
 
-// main game code?
+
+// main game code
 var codeGame = function() {
-    window.alert(""); // test code to see if function and click work - it does
+    // window.alert(""); // test code to see if function and click work - it does
+    // decrease time by one on button click (needs to be every second)
+    timeRemain = timeRemain - 1;
+    timer = document.getElementById("time").innerHTML = "<p>" + timeRemain + "</p>";
 }
 
-startButton.addEventListener("click", codeGame); // to click start to begin the game
+// to click start to begin the game
+startButton.addEventListener("click", codeGame); 
+
 
